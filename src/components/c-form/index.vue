6577e4d9c3555b6ae9d3baf8b55a431b5bc18fda
lints <LinTianSheng@newhope.cn>
2021-06-29 14:40:33 +0800

    添加表单组件实现

--- src/components/c-form/index.vue
+++ src/components/c-form/index.vue
@@ -1,9 +1,39 @@
 <template>
-  <h1>111</h1>
+  <form class="e-form">
+    <slot></slot>
+  </form>
 </template>
 
 <script>
 export default {
-  name: "form",
+  name: "c-form",
+  provide() {
+    // 跨层级传递参数，让子组件能访问到父组件的所有数据
+    return {
+      form: this,
+    };
+  },
+  props: {
+    model: {
+      type: Object,
+      require: true,
+    },
+    rules: {
+      type: Object,
+    },
+  },
+  methods: {
+    validate(cb) {
+      // 获取所有的孩子
+      // 获取到所有子元素的校验结果
+      const tasks = this.$children
+        .filter((item) => item.prop) //过滤调没有prop属性的item
+        .map((item) => item.validate());
+      console.log(tasks);
+      Promise.all(tasks)
+        .then(() => cb(true))
+        .catch(() => cb(false));
+    },
+  },
 };
 </script>
--- src/pages/form/index.vue
+++ src/pages/form/index.vue
@@ -1,9 +1,56 @@
 <template>
-  <div></div>
+  <div>
+    <c-form :model="userInfo" :rules="rules" ref="loginForm">
+      <c-form-item label="用户名：" prop="username">
+        <c-input v-model="userInfo.username" placeholder="请输入用户名" />
+      </c-form-item>
+      <c-form-item label="密码：" prop="password">
+        <c-input
+          v-model="userInfo.password"
+          type="password"
+          placeholder="请输入密码"
+        />
+      </c-form-item>
+      <c-form-item>
+        <button @click="login">登录</button>
+      </c-form-item>
+    </c-form>
+  </div>
 </template>
 
 <script>
+import CForm from "@/components/c-form";
+import CFormItem from "@/components/c-form/c-form-item.vue";
+import CInput from "@/components/c-form/c-input.vue";
 export default {
-  name: "form",
+  components: {
+    CForm,
+    CFormItem,
+    CInput,
+  },
+  data() {
+    return {
+      userInfo: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [{ required: true, message: "请输入用户名称" }],
+        password: [{ required: true, message: "请输入密码" }],
+      },
+    };
+  },
+  methods: {
+    login() {
+      this.$refs["loginForm"].validate((res) => {
+        console.log(res);
+        if (res) {
+          alert("验证通过");
+        } else {
+          alert("验证失败");
+        }
+      });
+    },
+  },
 };
 </script>
